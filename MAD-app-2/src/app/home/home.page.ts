6854dd9bdb2585e0b55958199e7e4ca863de1092
JensBax <thebaxfamily@MacBook-Pro-van-TheBaxFamily.local>
2019-10-10 09:25:12 +0200

    Camera and OCR

--- MAD-app-2/src/app/home/home.page.ts
+++ MAD-app-2/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera , CameraOptions, PictureSourceType} from '@ionic-native/camera/ngx'
+import { ActionSheetController } from '@ionic/angular';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  selectedImage: string
+  imageText: string
 
+  constructor(private camera: Camera, private actionSheetCtrl: ActionSheetController) {}
+
+  async selectSource(){
+    let actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPicture(sourceType: PictureSourceType){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then(imageData => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`
+    })
+  }
+
+  recognizeImage(){
+    Tesseract.recognize(this.selectedImage)
+    .catch(err => console.error(err))
+    .then(result => {
+      this.imageText = result.text
+    })
+  }
 }
